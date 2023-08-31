0458782aa953139d7d3b56343b586b1b63593170
neme-sis <ghoshsayan894@gmail.com>
2023-09-01 01:21:57 +0530

    backend configuration initial commit

--- server/routes/owner-api.js
+++ server/routes/owner-api.js
@@ -5,8 +5,8 @@ const registerUser = require("../controllers/owner-side-controllers/registerUser
 const getComments = require("../controllers/owner-side-controllers/getComments");
 const getAllInfo = require("../controllers/owner-side-controllers/getAllInfo");
 
-router.route("/register-user").post(registerUser);
-router.route("/comments").get(getComments);
-router.route("/all-info").get(getAllInfo);
+ownerApiRouter.route("/register-user").post(registerUser);
+ownerApiRouter.route("/comments").get(getComments);
+ownerApiRouter.route("/all-info").get(getAllInfo);
 
 module.exports = ownerApiRouter;
