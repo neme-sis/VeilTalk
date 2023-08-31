fc7de93bb31fdc4dc5012fc492f16f179e9e66a4
neme-sis <ghoshsayan894@gmail.com>
2023-09-01 03:18:12 +0530

    adding name and time stamp for commentor

--- server/controllers/user-side-controllers/addComment.js
+++ server/controllers/user-side-controllers/addComment.js
@@ -4,6 +4,14 @@ const {
   errorResponse,
 } = require("../../helper/defaultResponse");
 const { v4 } = require("uuid");
+const {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  countries,
+  names,
+  starWars,
+} = require("unique-names-generator");
 
 const addComment = async (req, res) => {
   const { user_id } = req.params;
@@ -20,9 +28,15 @@ const addComment = async (req, res) => {
         .json(errorResponse("", "", "Comments not found for the user"));
 
     const comment_id = v4();
+    const name = uniqueNamesGenerator({
+      dictionaries: [adjectives, colors, countries, names, starWars],
+      length: 2,
+      separator: " ",
+    });
+    const timestamp = new Date().getTime();
     await Comments.findOneAndUpdate(
       { user_id },
-      { $push: { comment_id, comment: req.body.comment } }
+      { $push: { comment_id, comment: req.body.comment, name, timestamp } }
     );
     return res.status(200).json(successResponse("Comment added successfully"));
   } catch (err) {
