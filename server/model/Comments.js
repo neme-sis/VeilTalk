b279a531dd96e18d67d2e9c37445082bc7176c76
neme-sis <ghoshsayan894@gmail.com>
2023-09-01 19:52:00 +0530

    sub schema defined

--- server/model/Comments.js
+++ server/model/Comments.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
+const EachComment = new mongoose.Schema({
+  comment_id: { type: String, required: true, unique: true },
+  comment: { type: String, required: true },
+  name: { type: String },
+  timestamp: { type: Number },
+});
+
 const Comments = new mongoose.Schema({
   user_id: { type: String, required: true, unique: true },
-  comments: { type: Array, default: [] },
+  comments: { type: EachComment, default: [] },
 });
 
 module.exports = mongoose.model("veiltalk-comments", Comments);
